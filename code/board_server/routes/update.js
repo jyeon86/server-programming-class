f8a815cf15eb69770dbd6dc21e946857d2b22606
Unknown <jrj8819@koreatech.ac.kr>
2018-06-22 00:21:07 +0900

    board_server 예제 추가

---
+++ code/board_server/routes/update.js
@@ -0,0 +1,15 @@
+// 필요한 모듈을 읽어오는 부분
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+// 미들웨어 부분
+// 주소를 localhost:3000/으로 접속한 경우, index.ejs파일을 웹브라우저에 출력해준다.
+// 이때 전달할 데이터를 담은 객체도 같이 보낸다,
+// title >> 키
+// 'Express' >> 값
+router.get('/update', function(req, res, next) {
+  res.render('update', { title: 'Express' });
+});
+
+module.exports = router;
