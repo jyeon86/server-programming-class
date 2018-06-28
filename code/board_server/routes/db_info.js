743d5a3ebda6b689c5bf98260cefd202925cb4bf
Unknown <jrj8819@koreatech.ac.kr>
2018-06-28 23:13:18 +0900

    .gitignore 파일 수정

--- code/board_server/routes/db_info.js
+++ code/board_server/routes/db_info.js
@@ -1,7 +1,7 @@
 var host = 'ec2-13-124-177-129.ap-northeast-2.compute.amazonaws.com';
 var port = 3306;
 var user = 'root';
-var password = 'raejin';
+var password = 'qwer1234';
 var database = 'student2';
 
 exports.getHost = function () {
