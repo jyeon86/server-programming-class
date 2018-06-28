11b92632729b3f9dabdce19cbc2dd07dcc20a686
Unknown <jrj8819@koreatech.ac.kr>
2018-06-29 00:25:31 +0900

    글쓰기시 공지 선택란 추가

--- code/board_server/routes/write.js
+++ code/board_server/routes/write.js
@@ -31,6 +31,8 @@ router.post('/', function(req, res, next) {
 	var name = req.body.nameInput;
 	var contents = req.body.contentsInput;
 	var category = req.body.categoryInput;
+	var noti = req.body.noti;
+	
 	console.log(title, name, contents, category);
 	// 데이터 베이스와 서버가 연결되기 시작
 	pool.getConnection(function(err, connection) {
@@ -40,11 +42,16 @@ router.post('/', function(req, res, next) {
 			throw err;
 		}
 		// sql문을 작성한다.(데이터베이스에 넣을 데이터와 sql문을 조합한 형태이다.)
+		/*
 		var sql = "INSERT INTO my_board (title, name, category,"+
 		" contents, update_at, create_at) VALUES "+
 		"('"+title+"', '"+name+"', '"+category+"', '"+contents+"', "+
 		"now(), now())";
-
+		*/	
+		var sql = "INSERT INTO my_board (title, name, category,"+
+		" contents, update_at, create_at, noti) VALUES "+
+		"('"+title+"', '"+name+"', '"+category+"', '"+contents+"', "+
+		"now(), now(), '"+noti+"')";
 		console.log(sql);
 		// 데이터베이스 연결을 통해서 sql문을 실행한다.
 		// 실행한 결과는 rows에 에러가 발생하면 err 객체에 정보가 나타난다.
