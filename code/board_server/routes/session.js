bdd3fb34c5e05286233f9fe9457dec0345ee158a
Unknown <jrj8819@koreatech.ac.kr>
2018-06-29 16:59:41 +0900

    로그인 기능 추가

--- code/board_server/routes/session.js
+++ code/board_server/routes/session.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var db_info = require('./db_info');
+
+var pool = mysql.createPool({
+    host : db_info.getHost(),
+    port : db_info.getPort(),
+    user : db_info.getUser(),
+    password : db_info.getPassword(),
+    database : db_info.getDatabase(),
+    connectionLimit : 20,
+    waitForConnections : false
+});
 
 router.get('/login', function(req, res, next) {
     console.log("login site");
@@ -10,11 +22,27 @@ router.post('/login', function(req, res, next) {
 	var id = req.body.id;
 	var password = req.body.password;
 	if(!req.session.user) {
-		req.session.user = { id : id };
-	}
-	console.dir(req.session);
+		pool.getConnection(function(err, connection) {
+			var sql = "SELECT * FROM user_info WHERE id= '" 
+				+ id +"' AND password=PASSWORD('"+password+"')";
 
-	res.redirect('http://localhost:3000/');
+			 console.log(sql)
+			 var query = connection.query(sql, function(err, rows) {
+				if(err) {
+					connection.release();
+					throw err;
+				}
+				console.dir(rows);
+				if(rows.length == 0) {
+					res.redirect("http://localhost:3000/session/login")
+				} else {
+					req.session.user = { id : id };
+					res.redirect('http://localhost:3000/');
+				}
+				connection.release();
+			});
+		});
+	}
 });
 
 router.get('/logout', function(req, res, next) {
