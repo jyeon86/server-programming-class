9a0efd60a18e315715fa435a9f0e6fa9a49cf916
Unknown <jrj8819@koreatech.ac.kr>
2018-07-14 20:33:22 +0900

    검색 파트에 페이지 네이션 적용

--- code/board_server/routes/search.js
+++ code/board_server/routes/search.js
@@ -14,17 +14,24 @@ var pool = mysql.createPool({
     waitForConnections : false
 });
 
-router.get('/title/:keyword/:number_of_page', function(req, res, next) {
+router.get('/:search_type/:keyword/:number_of_page', function(req, res, next) {
+    var searchType = req.params.search_type;
     var keyword = req.params.keyword;
     var number_of_page = req.params.number_of_page;
+    console.log("searchType : " + searchType)
+    if(searchType == "writer") {
+        searchType = "name";
+    } else if (searchType == "number") {
+        searchType = "_idx";
+    }
     pool.getConnection(function(err, connection) {
         var sql_count = "SELECT count(*) AS count " +
                         "FROM my_board WHERE enable=1 " +
-                        "AND title like '%" + keyword + "%'"; 
+                        "AND " + searchType+ " like '%" + keyword + "%'"; 
         console.log(sql_count);
 
         var sql = "SELECT * FROM my_board WHERE enable=1 " +
-                "AND title like '%" + keyword + "%'" +
+                "AND "+searchType+" like '%" + keyword + "%'" +
                 "ORDER BY create_at DESC " +
                 "LIMIT 3 OFFSET " + ((number_of_page-1)*3);
         console.log(sql);
@@ -41,11 +48,17 @@ router.get('/title/:keyword/:number_of_page', function(req, res, next) {
                     throw err;
                 }
 
+                if(searchType == "name") {
+                    searchType = "writer";
+                } else if (searchType == "_idx") {
+                    searchType = "number";
+                }
+
                 if(req.session.user) {
                     res.render('index', 
                         { rows : rows, is_logined : true, 
                             login_id : req.session.user.user_id,
-                            type : "title",
+                            type : searchType,
                             search_keyword : keyword,
                             total_write : total_write });
                         
@@ -53,7 +66,7 @@ router.get('/title/:keyword/:number_of_page', function(req, res, next) {
                     res.render('index', 
                         { rows : rows, is_logined : false,
                             login_id : "",
-                            type : "title",
+                            type : searchType,
                             search_keyword : keyword,
                             total_write : total_write });   
                 }
@@ -68,61 +81,79 @@ router.post('/', function(req, res, next) {
     var keyword = req.body.keywordInput;
     var searchType = req.body.searchTypeInput;
 
+    if(searchType == "writer") {
+        searchType = "name";
+    } else if (searchType == "number") {
+        searchType = "_idx";
+    }
+    
     pool.getConnection(function(err, connection) {
         if(err) {
             console.log("getConnection Error");
             throw err;
         }
-        var sql = 'SELECT * FROM my_board WHERE ';
+        var sql_count = "SELECT count(*) AS count " +
+                        "FROM my_board WHERE enable=1 " +
+                        "AND "+searchType+" like '%" + keyword + "%'"; 
+        console.log(sql_count);
 
-        switch(searchType) {
-            case 'title':
-                sql += "title like '%" + keyword + "%';"
-                break;
-            case 'writer':
-                sql += "name like '%" + keyword + "%';"
-                break;
-            case 'number':
-                sql += "_idx = " + keyword + ";"
-                break;
-        }
-        console.log(sql);
-
-        var query = connection.query(sql, function(err, rows) {
+        connection.query(sql_count, function(err, rows) {
             if(err) {
-                console.log("query Error");
                 connection.release();
                 throw err;
             }
-            console.dir(rows);
-            /*
-            if(req.session.user) {
-                res.render('index', { rows : rows, is_logined : true, 
-                    login_id : req.session.user.user_id });
-                    
-            } else {
-                res.render('index', { rows : rows, is_logined : false, login_id : "" });   
-            }
-            */
-            if(req.session.user) {
-                res.render('index', 
-                    { rows : rows, is_logined : true, 
-                        login_id : req.session.user.user_id,
-                        type : searchType,
-                        search_keyword : keyword,
-                        total_write : rows.length });
-                    
-            } else {
-                res.render('index', 
-                    { rows : rows, is_logined : false,
-                        login_id : "",
-                        type : searchType,
-                        search_keyword : keyword,
-                        total_write : rows.length });   
-            }
-            connection.release();
-        });
+            var total_write = rows[0].count;
+            console.log(total_write);
 
+            var sql = 'SELECT * FROM my_board WHERE ';
+
+            if(searchType == "name") {
+                searchType = "writer";
+            } else if (searchType == "_idx") {
+                searchType = "number";
+            }
+            switch(searchType) {
+                case 'title':
+                    sql += "title like '%" + keyword + "%' "
+                    break;
+                case 'writer':
+                    sql += "name like '%" + keyword + "%' "
+                    break;
+                case 'number':
+                    sql += "_idx = " + keyword + " "
+                    break;
+            }
+
+            sql += "ORDER BY create_at DESC ";
+            sql += "LIMIT 3 OFFSET 0";
+            console.log(sql);
+
+            var query = connection.query(sql, function(err, rows) {
+                if(err) {
+                    console.log("query Error");
+                    connection.release();
+                    throw err;
+                }
+                
+                if(req.session.user) {
+                    res.render('index', 
+                        { rows : rows, is_logined : true, 
+                            login_id : req.session.user.user_id,
+                            type : searchType,
+                            search_keyword : keyword,
+                            total_write : total_write });
+                        
+                } else {
+                    res.render('index', 
+                        { rows : rows, is_logined : false,
+                            login_id : "",
+                            type : searchType,
+                            search_keyword : keyword,
+                            total_write : total_write });   
+                }
+                connection.release();
+            });
+        });
     });
 });
 
